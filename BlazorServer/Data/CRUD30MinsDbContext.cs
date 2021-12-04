ec37010351f299e769275ef1cc3bfa62d16689da
Cullington <70056723+RubenHeeren@users.noreply.github.com>
2021-12-04 14:49:45 +0100

    Add project files.

---
+++ BlazorServer/Data/CRUD30MinsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServer.Data;
+
+internal sealed class CRUD30MinsDbContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=CRUD30Mins.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Post[] postsToSeed = new Post[6];
+
+        for (int i = 1; i <= 6; i++)
+        {
+            postsToSeed[i - 1] = new Post
+            {
+                PostId = i,
+                Title = $"Post {i}",
+                Content = $"This is post {i}'s content. I have also liked and subscribed."
+            };
+        }
+
+        modelBuilder.Entity<Post>().HasData(postsToSeed);
+    }
+}
---
+++ BlazorServer/Data/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServer.Data;
+
+internal sealed class Post
+{
+    [Key]
+    public int PostId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(5000)]
+    public string Content { get; set; } = string.Empty;
+}
